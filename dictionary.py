8feba70759bf4fab331bf99487810538c247fdbb
Peter Yang <fgdeeerry@gmail.com>
2021-02-01 21:48:38 +0800

    secondary commit

--- dictionary.py
+++ dictionary.py
@@ -8,3 +8,20 @@ words = {
 
 words['tea'] = '茶'
 print(words)
+
+
+p0 = {
+	'name': '珍珠紅茶',
+	'price': 30,
+	'Remarks': '微冰微糖'
+}
+
+p1 = {
+	'name': '蜂蜜綠',
+	'price': 35,
+	'Remarks': '微冰半蜜'
+}
+
+drinks = [p0, p1]
+print(drinks[0])
+print(drinks[1]['Remarks'])
